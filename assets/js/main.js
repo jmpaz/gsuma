abf6ab3c094664460300e499d9c7eba22681f453
Franklin Pazmino <0jfpz0@gmail.com>
2017-10-21 14:47:27 -0400

    Replace map.html with magific-popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -612,6 +612,23 @@ var parallelism = (function($) { var _ = {
 								});
 
 						});
+						
+						//map popup
+						$('.map').magnificPopup({
+							type: 'iframe',
+												
+							iframe: {
+							   markup: '<div class="mfp-iframe-scaler">'+
+										  '<div class="mfp-close"></div>'+
+										  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+										'</div>'
+							},
+							callbacks: {
+							  markupParse: function(template, values, item) {
+							   values.title = item.el.attr('title');
+							  }
+							}
+						});
 
 				});
 
